056ed84361853173045a59748c2b36b92093232a
DESKTOP-NVAVOMS\botez <botezatu.david91@gmail.com>
2022-06-09 19:51:57 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestiuneBileteAvion.Startup))]
+namespace GestiuneBileteAvion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
